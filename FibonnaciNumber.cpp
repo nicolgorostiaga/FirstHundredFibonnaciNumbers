8a5652f1f5de2688087f6dbfcaf7a878e82d816b
nicolgorostiaga <nicolgorostiaga@gmail.com>
2016-12-14 11:41:22 -0600

    FirstHundred FibonnaciNumbers in C++

---
+++ FibonnaciNumber.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+void firstHundred(int previousNumber, int nextNumber);
+int main(){
+
+	int previousNumber = 0;// initiate first Fibonnaci number
+	int nextNumber = 1; // initiate second Fibonnaci number
+	firstHundred(previousNumber,nextNumber);
+}
+//*******************************************************************
+void firstHundred(int previousNumber, int nextNumber){
+	cout <<previousNumber<< " ";
+	cout <<nextNumber<< " ";
+	for(int loop =0; loop <=98; loop++){
+		int FibonnaciNumber = nextNumber + previousNumber;
+		previousNumber = nextNumber;
+		nextNumber= FibonnaciNumber;
+		cout << FibonnaciNumber<< " ";
+	}
+}
